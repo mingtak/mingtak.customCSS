76ef3b405ce56eaac6a2bf22d3efae20c41b8f88
Andy Fang <andy@mingtak.com.tw>
2018-08-15 03:47:49 +0000

    update version no.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='mingtak.customCSS',
-    version='1.0a1',
+    version='1.0',
     description="Custom CSS file online",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
